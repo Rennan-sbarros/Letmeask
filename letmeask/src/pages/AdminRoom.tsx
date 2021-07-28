7443dc602c6751bed91c591b43ed20515d597348
Rennan Candido <rennan_sbarros@hotmail.com>
2021-07-27 23:32:48 -0300

    question removal functionality

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -5,8 +5,10 @@ import { useParams } from 'react-router-dom';
 //import { useAuth } from '../hooks/useAuth';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+import deleteImg from '../assets/images/delete.svg';
 
 import '../styles/room.scss';
+import { database } from '../services/firebase';
 
 type RoomParams = {
     id: string;
@@ -18,6 +20,12 @@ export function AdminRoom(){
     const roomId = params.id;
 
     const { title, questions} = useRoom(roomId);
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que você deseja excluir esta pergunta')){
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return(
         <div id="page-room">
@@ -44,7 +52,14 @@ export function AdminRoom(){
                                 key={question.id} //Identifica a diferença de uma pergunta para outra pelo id para performance. *Algoritmo de reconciliação 
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remover perguntas"/>
+                                </button>
+                            </Question>
                         );
                     })}
                 </div> 
